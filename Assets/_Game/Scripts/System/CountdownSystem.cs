9aaa9789d63963757e8a319face39b05e7649ab4
nguyennt02 <nguyennt19apr@gmail.com>
2024-12-19 00:17:03 +0700

    add countdown

--- Assets/_Game/Scripts/System/CountdownSystem.cs
+++ Assets/_Game/Scripts/System/CountdownSystem.cs
@@ -1,16 +1,57 @@
+using System;
 using UnityEngine;
 
 public class CountdownSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static CountdownSystem Instance {get; private set;}
+    [SerializeField] int maxTimeInSeconds = 1200;
+    double elapsedtimeSecounds;
+    public double ElapsedtimeSecounds { get; private set; }
+    public Action<int> OnEndLoopTimeSpin;
+    bool isPause = true;
+    DateTime startTime;
     void Start()
     {
-        
+        Instance = this;
+        if (PlayerPrefs.HasKey(KeyString.KEY_EXIt_TIME))
+        {
+            long exitTime = long.Parse(PlayerPrefs.GetString(KeyString.KEY_EXIt_TIME));
+            startTime = DateTime.FromBinary(exitTime);
+        }
+        else
+        {
+            startTime = DateTime.Now;
+        }
+    }
+    public void StartCountdown()
+    {
+        startTime = DateTime.Now;
+        PlayerPrefs.SetString(KeyString.KEY_EXIt_TIME, startTime.ToBinary().ToString());
+        isPause = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StopCountdown()
     {
-        
+        isPause = true;
+    }
+
+    void FixedUpdate()
+    {
+        LoopTime();
+    }
+
+    void LoopTime()
+    {
+        if (isPause) return;
+        TimeSpan elapsedtime = DateTime.Now - startTime;
+        elapsedtimeSecounds = elapsedtime.TotalSeconds;
+        if (elapsedtimeSecounds > maxTimeInSeconds)
+        {
+            int amountSpin = (int)elapsedtimeSecounds / maxTimeInSeconds;
+            double timeRmaining = (double)elapsedtimeSecounds % maxTimeInSeconds;
+            OnEndLoopTimeSpin?.Invoke(amountSpin);
+            StartCountdown();
+            startTime -= TimeSpan.FromSeconds(timeRmaining);
+        }
     }
 }
